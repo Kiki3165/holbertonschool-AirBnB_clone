ffacac272cf7df7f46b9614a8a258e5f8758c4df
kiki3165 <kikikou82@gmail.com>
2023-02-20 15:31:27 +0000

    delete commment

--- models/base_model.py
+++ models/base_model.py
@@ -26,7 +26,7 @@ class BaseModel:
     def to_dict(self):
         '''def to_dict'''
         obj_dict = self.__dict__.copy()
-        obj_dict['__class__'] = type(self).__name__
-        obj_dict['created_at'] = self.created_at.isoformat()
-        obj_dict['updated_at'] = self.updated_at.isoformat()
+        obj_dict = type(self).__name__
+        obj_dict = self.created_at.isoformat()
+        obj_dict = self.updated_at.isoformat()
         return obj_dict
