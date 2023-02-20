6d3211dc89ef495aede73dc4a6275d6972e269c3
kiki3165 <kikikou82@gmail.com>
2023-02-20 15:33:57 +0000

    delete commment

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ from datetime import datetime
 class BaseModel:
     
     def __init__(self, id):
-        '''constructor'''
+        '''def init'''
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
