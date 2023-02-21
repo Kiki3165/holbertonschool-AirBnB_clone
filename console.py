ff2f53b08574a1644e9ad527bb6cadf302affb47
Meashyshy <camillefvrl@gmail.com>
2023-02-21 06:52:31 -0800

    Basic Console

--- console.py
+++ console.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
-'''simple def'''
+"""Console"""
+import cmd
 
 
-class hbnb:
-    pass
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter class"""
+
+    prompt = '(hbnb) '
+
+    def do_quit(self, line):
+        """Quit command"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command"""
+        return True
+
+    def emptyline(self):
+        """Do nothing if empty line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
