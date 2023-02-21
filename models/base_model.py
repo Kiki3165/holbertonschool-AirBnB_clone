bd9b539bd1a2b1273fb446092233adffe6a78ab6
kiki3165 <kikikou82@gmail.com>
2023-02-21 15:19:05 +0000

    update init with args and kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -9,16 +9,20 @@ from datetime import datetime
 
 class BaseModel:
     
-    def __init__(self, id=None):
-        '''def init'''
-        if id is None:
-            id = {}
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
+            self.id = kwargs.get('id', str(uuid.uuid4()))
+            self.created_at = kwargs.get('created_at', datetime.now())
+            self.updated_at = datetime.now()
         else:
-            self.id = id
-
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''def str'''
