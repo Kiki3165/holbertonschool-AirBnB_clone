dfa48bc1437519533c38a1adcb0387abcd53cfae
kiki3165 <kikikou82@gmail.com>
2023-02-22 12:05:08 +0000

    add save method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,10 +14,16 @@ class FileStorage:
         '''simple def'''
         return FileStorage.__objects
 
-    def new(self, obj)
+    def new(self, obj):
         '''simple def'''
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
     def save(self):
         '''simple def'''
+        obj_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            obj_dict[key] = obj.to_dict()
+
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            json.dump(obj_dict, f, indent=4)
