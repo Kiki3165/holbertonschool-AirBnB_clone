6a394513c41316746cf5e7bc1d216dd266441415
Meashyshy <camillefvrl@gmail.com>
2023-02-23 05:23:59 -0800

    Small changes

--- console.py
+++ console.py
@@ -3,6 +3,12 @@
 import cmd
 import json
 
+from models.user import User
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
@@ -12,7 +18,7 @@ class HBNBCommand(cmd.Cmd):
     """Command interpreter class"""
 
     prompt = '(hbnb) '
-    classes = {"BaseModel"}
+    classes = {"BaseModel", "User", "Amenity", "City", "Review", "State", "Place"}
 
     def do_quit(self, line):
         """Quit command"""
