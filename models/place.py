440b64e410103244d3cfdb72e821a7099206c297
Meashyshy <camillefvrl@gmail.com>
2023-02-23 05:18:03 -0800

    Created all classes

---
+++ models/place.py
@@ -0,0 +1,16 @@
+"""class Place"""
+from base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Place attributes"""
+    city_id: str = ""
+    user_id: str = ""
+    name: str = ""
+    description: str =""
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = float(0.0)
+    longitude = float(0.0)
+    amenity_ids = []
