4ac63dd0e28a5d7f4062fe95416f4bd3d75bf007
Meashyshy <camillefvrl@gmail.com>
2023-02-23 05:31:48 -0800

    Added missing variable

--- models/place.py
+++ models/place.py
@@ -7,7 +7,8 @@ class Place(BaseModel):
     city_id: str = ""
     user_id: str = ""
     name: str = ""
-    description: str =""
+    description: str = ""
+    number_rooms = 0
     number_bathrooms = 0
     max_guest = 0
     price_by_night = 0
