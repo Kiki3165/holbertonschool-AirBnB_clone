36c274b4f7ae6dd6b0b361c5803d2f8854b16e83
kiki3165 <kikikou82@gmail.com>
2023-02-24 22:54:34 +0000

    fix all files

--- models/base_model.py
+++ models/base_model.py
@@ -9,6 +9,7 @@ from models import storage
 
 class BaseModel:
     def __init__(self, *args, **kwargs):
+        '''def'''
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -21,13 +22,16 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        '''def'''
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        '''def'''
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        '''def'''
         dictionary = dict(self.__dict__)
         dictionary['__class__'] = self.__class__.__name__
         dictionary['created_at'] = self.created_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,13 +10,16 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        '''def'''
         return self.__objects
 
     def new(self, obj):
+        '''def'''
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
+        '''def'''
         new_dict = {}
         for key, value in self.__objects.items():
             new_dict[key] = value.to_dict()
@@ -24,6 +27,7 @@ class FileStorage:
             json.dump(new_dict, f)
 
     def reload(self):
+        '''def'''
         try:
             with open(self.__file_path, 'r') as f:
                 FileStorage.__objects = json.load(f)
